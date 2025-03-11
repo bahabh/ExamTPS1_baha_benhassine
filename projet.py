566082fc4cab5f2575dfe04b7f88166624b43257
G4tou3 <G4tou3@gmail.com>
2025-03-11 12:22:39 +0100

    3eme ajout au projet en main

--- projet.py
+++ projet.py
@@ -7,5 +7,14 @@ def somme(T):
 
 Data=[1,3,5]
 Som=sum(Data)
+
 print ("la somme est :" , som)
+
+
+if Data :
+	print ("la somme est :" , sum(Data))
+	print ("la min est :" , min(Data))
+	print ("la max est :" , max(Data))
+else :
+	print ("dossier vide")
 
